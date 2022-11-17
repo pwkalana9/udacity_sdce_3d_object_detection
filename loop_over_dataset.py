b3366f42dd51a89e5360b5f1e6157603a09cd4fa
Kalana <pwkalana9@gamil.com>
2022-11-17 23:39:09 +0000

    range image added

--- loop_over_dataset.py
+++ loop_over_dataset.py
@@ -52,8 +52,9 @@ import misc.params as params
 # data_filename = 'training_segment-1005081002024129653_5313_150_5333_150_with_camera_labels.tfrecord' # Sequence 1
 # data_filename = 'training_segment-10072231702153043603_5725_000_5745_000_with_camera_labels.tfrecord' # Sequence 2
 data_filename = 'training_segment-1005081002024129653_5313_150_5333_150_with_camera_labels.tfrecord' # Sequence 3
+# data_filename = 'training_segment-10963653239323173269_1924_000_1944_000_with_camera_labels.tfrecord' # Sequence 3
 # data_filename = 'training_segment-1005081002024129653_5313_150_5333_150_with_camera_labels.tfrecord'
-#show_only_frames = [0, 1] # show only frames in interval for debugging
+# show_only_frames = [0, 1] # show only frames in interval for debugging
 show_only_frames = [50, 51]
 
 ## Prepare Waymo Open Dataset file for loading
@@ -70,11 +71,11 @@ datafile = WaymoDataFileReader(data_fullpath)
 datafile_iter = iter(datafile)  # initialize dataset iterator
 
 ## Initialize object detection
-# configs_det = det.load_configs(model_name='fpn_resnet')#'darknet') # options are 'darknet', 'fpn_resnet'
+#configs_det = det.load_configs(model_name='fpn_resnet')#'darknet') # options are 'darknet', 'fpn_resnet'
 configs_det = det.load_configs(model_name='darknet')
 model_det = det.create_model(configs_det)
 
-configs_det.use_labels_as_objects = False # True = use groundtruth labels as objects, False = use model-based detection
+configs_det.use_labels_as_objects = True # True = use groundtruth labels as objects, False = use model-based detection
 
 ## Uncomment this setting to restrict the y-range in the final project
 # configs_det.lim_y = [-25, 25] 
@@ -88,17 +89,18 @@ camera = None # init camera sensor object
 np.random.seed(10) # make random values predictable
 
 ## Selective execution and visualization
-# exec_data = ['pcl_from_rangeimage', 'load_image']#['pcl_from_rangeimage']
+exec_data = ['pcl_from_rangeimage', 'load_image']#['pcl_from_rangeimage']
 # exec_detection = ['bev_from_pcl', 'detect_objects']#['bev_from_pcl']
 # exec_detection = ['bev_from_pcl', 'detect_objects', 'validate_object_labels', 'measure_detection_performance'] # options are 'bev_from_pcl', 'detect_objects', 'validate_object_labels', 'measure_detection_performance'; options not in the list will be loaded from file
 # exec_tracking = [] # options are 'perform_tracking'
 # exec_visualization = ['show_objects_in_bev_labels_in_camera']#[] #['show_range_image']# ['show_pcl'] # options are 'show_range_image', 'show_bev', 'show_pcl', 'show_labels_in_image', 'show_objects_and_labels_in_bev', 'show_objects_in_bev_labels_in_camera', 'show_tracks', 'show_detection_performance', 'make_tracking_movie'
-
-exec_data = ['pcl_from_rangeimage']#['pcl_from_rangeimage']
-
-exec_detection = ['bev_from_pcl', 'detect_objects', 'validate_object_labels', 'measure_detection_performance']#['bev_from_pcl']
-# exec_detection = ['bev_from_pcl', 'detect_objects', 'validate_object_labels', 'measure_detection_performance'] # options are 'bev_from_pcl', 'detect_objects', 'validate_object_labels', 'measure_detection_performance'; options not in the list will be loaded from file
+# exec_data = []
+# exec_data = ['pcl_from_rangeimage']#['pcl_from_rangeimage']
+#exec_detection = ['bev_from_pcl', 'detect_objects']
+#exec_detection = ['bev_from_pcl', 'detect_objects', 'validate_object_labels', 'measure_detection_performance']#['bev_from_pcl']
+exec_detection = ['bev_from_pcl', 'detect_objects', 'validate_object_labels', 'measure_detection_performance'] # options are 'bev_from_pcl', 'detect_objects', 'validate_object_labels', 'measure_detection_performance'; options not in the list will be loaded from file
 exec_tracking = [] # options are 'perform_tracking'
+#exec_visualization = ['show_objects_in_bev_labels_in_camera']#['show_pcl']
 exec_visualization = ['show_detection_performance']#[] #['show_range_image']# ['show_pcl'] # options are 'show_range_image', 'show_bev', 'show_pcl', 'show_labels_in_image', 'show_objects_and_labels_in_bev', 'show_objects_in_bev_labels_in_camera', 'show_tracks', 'show_detection_performance', 'make_tracking_movie'
 
 exec_list = make_exec_list(exec_detection, exec_tracking, exec_visualization)
